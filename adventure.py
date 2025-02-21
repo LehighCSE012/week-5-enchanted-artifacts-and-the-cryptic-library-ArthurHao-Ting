b39a9d1204d17c91436c31fd5c11f9f2f935e823
ArthurHao-Ting <tih228@lehigh.edu>
2025-02-20 21:50:40 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -147,7 +147,7 @@ def main():
             artifact_name = random.choice(list(artifacts.keys()))
             player_stats, artifacts = discover_artifact(player_stats, artifacts, artifact_name)
             display_player_status(player_stats)
- 
+
         if player_stats['health'] > 0:
             player_stats, inventory, clues = enter_dungeon\
                 (player_stats, inventory, dungeon_rooms, clues)
